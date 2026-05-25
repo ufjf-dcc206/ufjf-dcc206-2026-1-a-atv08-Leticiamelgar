7d64b952e8066183548f5612f5f57f21a22a6994
Leticia Melgar Floro <leticia.melgar@estudante.ufjf.br>
2026-05-25 17:05:06 -0300

    :sparkles: Adiciona métodos de acesso

--- dcc206-atv8/src/StarRating.js
+++ dcc206-atv8/src/StarRating.js
@@ -1,10 +1,22 @@
 class StarRating extends HTMLElement {
     
-    stars = 1;
+    #stars = 1;
     
     constructor() {
         super();
         console.log("Construtor da StarRating executou!");
+        this.stars = 3;
+    }
+
+    get stars() {
+        return this.#stars;
+    }
+
+    set stars(novoValor) {
+        if(1<= novoValor && novoValor <= 5) {
+            this.#stars = novoValor;
+        }
+        this.textContent = "";
         for (let i = 0; i < this.stars; i++) {
             this.textContent += "⭐";
         }
