0da07b3f774f5113c9034a7ee78003e738576df6
Leticia Melgar Floro <leticia.melgar@estudante.ufjf.br>
2026-05-25 17:00:24 -0300

    :sparkles: Adiciona propriedade para estrelas

--- dcc206-atv8/src/StarRating.js
+++ dcc206-atv8/src/StarRating.js
@@ -1,7 +1,13 @@
 class StarRating extends HTMLElement {
+    
+    stars = 1;
+    
     constructor() {
         super();
         console.log("Construtor da StarRating executou!");
+        for (let i = 0; i < this.stars; i++) {
+            this.textContent += "⭐";
+        }
     }
 }
 
